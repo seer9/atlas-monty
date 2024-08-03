a054a72041863ef72d76244371625ebd3777df5c
Clay <rpctropic13@gmail.com>
2024-08-03 19:37:58 +0000

    check

--- instructions.c
+++ instructions.c
@@ -1,29 +1,45 @@
 #include "monty.h"
-#include <string.h>
 
-typedef void (*opcode_func_t)(stack_t **stack, unsigned int line_number, char *arg);
-
+/**
+ * exe_instruction - executes the given opcode
+ * @opcode: string representing the opcode
+ * @arg: argument for the opcode (if any)
+ * @stack: pointer to the head of the stack
+ * @line_number: the current line number
+ */
 void exe_instruction(char *opcode, char *arg, stack_t **stack, unsigned int line_number)
 {
-    int i;
-    char *opcodes[] = {"push", "pall", "pint", "pop", "swap", "add", "nop", NULL};
-    opcode_func_t functions[] = {
-        push,
-        (opcode_func_t) pall,
-        (opcode_func_t) pint,
-        (opcode_func_t) pop,
-        (opcode_func_t) swap,
-        (opcode_func_t) add,
-        (opcode_func_t) nop
-    };
-    for (i = 0; opcodes[i] != NULL; i++)
+    if (strcmp(opcode, "push") == 0)
     {
-        if (strcmp(opcodes[i], opcode) == 0)
-        {
-            functions[i](stack, line_number, arg);
-            return;
-        }
+        push(stack, line_number, arg);
     }
-    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-    exit(EXIT_FAILURE);
+    else if (strcmp(opcode, "pall") == 0)
+    {
+        pall(stack, line_number);
+    }
+    else if (strcmp(opcode, "pint") == 0)
+    {
+        pint(stack, line_number);
+    }
+    else if (strcmp(opcode, "pop") == 0)
+    {
+        pop(stack, line_number);
+    }
+    else if (strcmp(opcode, "swap") == 0)
+    {
+        swap(stack, line_number);
+    }
+    else if (strcmp(opcode, "add") == 0)
+    {
+        add(stack, line_number);
+    }
+    else if (strcmp(opcode, "nop") == 0)
+    {
+        nop(stack, line_number);
+    }
+    else
+    {
+        fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+        exit(EXIT_FAILURE);
+    }
 }
