69e71557ce79513a62573a89c47d392a2b85d063
Clay <rpctropic13@gmail.com>
2024-08-03 22:18:00 +0000

    check

--- main.c
+++ main.c
@@ -3,47 +3,54 @@
 /**
  * main - Entry point
  *
- * @argc: Number of arguments
- * @argv: Array of arguments
+ * argc: number of arguments
+ * argv: array of arguments
+ * Return: 0 on success, 1 on failure
  */
+
 int main(int argc, char **argv)
 {
+    stack_t *stack = NULL;
     FILE *file;
     char *line = NULL;
     size_t len = 0;
-    ssize_t read;
     unsigned int line_number = 0;
-    stack_t *stack = NULL;
+
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
+
     file = fopen(argv[1], "r");
-    if (file == NULL)
+    if (!file)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    while ((read = getline(&line, &len, file)) != -1)
+
+    while (getline(&line, &len, file) != -1)
     {
         line_number++;
-        char *opcode = strtok(line, " \n\t\r");
-        char *arg = strtok(NULL, " \n\t\r");
-        if (opcode == NULL)
-            continue;
-        
-        exe_instruction(opcode, arg, &stack, line_number);
+        char *opcode = strtok(line, " \t\n");
+        char *arg = strtok(NULL, " \t\n");
+
+        if (opcode && opcode[0] != '#')  // Ignore empty lines and comments
+        {
+            exe_instruction(opcode, arg, &stack, line_number);
+        }
     }
+
     free(line);
     fclose(file);
-    free_stack(stack);
+    free_stack(stack);  // Ensure all stack elements are freed
     return (0);
 }
 
 void free_stack(stack_t *stack)
 {
     stack_t *temp;
+
     while (stack)
     {
         temp = stack;
