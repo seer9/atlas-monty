d949ccc7d484c2c1c8cf8d2bc60f76e7538cf159
Clay <rpctropic13@gmail.com>
2024-08-03 23:43:18 +0000

    checking line len

--- monty.h
+++ monty.h
@@ -47,7 +47,8 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-void exe_instruction(char *opcode, char *arg, stack_t **stack, unsigned int line_number);
+void exe_instruction(char *opcode, char *arg, stack_t **stack,
+unsigned int line_number);
 void free_stack(stack_t *stack);
 int is_integer(const char *str);
 
