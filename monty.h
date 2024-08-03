cbb85cf878dda2a33a9dee507259aefe9cd87d52
Clay <rpctropic13@gmail.com>
2024-08-03 23:33:08 +0000

    function declaration

--- monty.h
+++ monty.h
@@ -39,17 +39,16 @@ typedef struct instruction_s
 extern char *push_arg;
 
 /* Function Prototypes */
-void exe_instruction(char *opcode, char *arg, stack_t **stack, unsigned int line_number);
-int is_integer(const char *str);
 void push(stack_t **stack, unsigned int line_number, char *arg);
+void push_wrapper(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void exe_instruction(char *opcode, char *arg, stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
-void push_wrapper(stack_t **stack, unsigned int line_number);
-
+int is_integer(const char *str);
 
 #endif
