e051df28867e27b1be501f6a36c471a8d6ba07d2
Clay <rpctropic13@gmail.com>
2024-08-03 18:29:36 +0000

    check

--- monty.c
+++ monty.c
@@ -40,41 +40,10 @@ int main(int argc, char *argv[])
         if (opcode == NULL || opcode[0] == '#')
             continue;
 
-        execute_instruction(opcode, arg, &stack, line_number);
+        exe_instruction(opcode, arg, &stack, line_number);
     }
 
     free(line);
     fclose(file);
     return (0);
-}
-
-/**
- * exe_instruction - Executes the instruction
-
- * @opcode: The opcode
- * @arg: The argument
- * @stack: Pointer to the stack
- * @line_number: The line number
- */
-
-void exe_instruction(char *opcode, char *arg, stack_t **stack, unsigned int line_number)
-{
-    int i;
-    instruction_t instructions[] = {
-        {"push", (void (*)(stack_t **, unsigned int))push},
-        {"pall", pall},
-        {NULL, NULL}
-    };
-
-    for (i = 0; instructions[i].opcode != NULL; i++)
-    {
-        if (strcmp(opcode, instructions[i].opcode) == 0)
-        {
-            instructions[i].f(stack, line_number);
-            return;
-        }
-    }
-
-    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-    exit(EXIT_FAILURE);
 }
