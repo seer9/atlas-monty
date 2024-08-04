8ce5f5bf1068d7fcdcd24a5fdf83cc17be18b967
root <rpctropic13@gmail.com>
2024-08-04 00:11:12 +0000

    one global variable pls (:

--- main.c
+++ main.c
@@ -10,41 +10,41 @@ char *push_arg; /* global variable to store the push argument */
  */
 int main(int argc, char **argv)
 {
-    stack_t *stack = NULL;
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    unsigned int line_number = 0;
+	stack_t *stack = NULL;
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while (getline(&line, &len, file) != -1)
-    {
-        line_number++;
-        char *opcode = strtok(line, " \t\n");
-        char *arg = strtok(NULL, " \t\n");
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		char *opcode = strtok(line, " \t\n");
+		char *arg = strtok(NULL, " \t\n");
 
-        if (opcode && opcode[0] != '#')  /* Ignore empty lines and comments */
-        {
-            exe_instruction(opcode, arg, &stack, line_number);
-        }
-    }
+		if (opcode && opcode[0] != '#')  /* Ignore empty lines and comments */
+		{
+			exe_instruction(opcode, arg, &stack, line_number);
+		}
+	}
 
-    free(line);
-    fclose(file);
-    free_stack(stack);  /* Ensure stack is freed properly */
-    return (0);
+	free(line);
+	fclose(file);
+	free_stack(stack);  /* Ensure stack is freed properly */
+	return (0);
 }
 /**
  * free_stack - Frees the memory allocated for the stack
@@ -52,12 +52,12 @@ int main(int argc, char **argv)
  */
 void free_stack(stack_t *stack)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    while (stack)
-    {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+	while (stack)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
 }
